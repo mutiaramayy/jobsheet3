ac9062b1893e2311fc4a1ca7e8bdcf12487e3b85
Mutiara Inayah Maylafaiza <mutiaramaylafaiza@gmail.com>
2024-10-04 10:20:57 +0700

    Add files via upload

---
+++ ContohVariabel21.java
@@ -0,0 +1,14 @@
+public class ContohVariabel21 {
+    public static void main (String[] args) {
+        String SalahSatuHobySayaAdalah = "Traveling";
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 18;
+        double $ipk = 3.90, tinggi = 1.53;
+        System.out.println(SalahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
